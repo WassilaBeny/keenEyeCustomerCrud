f9ba62502e2140fc26eb5788dc24723bf455374b
Wassila <benyahia.wassila.dev>
2021-11-01 17:48:32 +0100

    generate customer resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from "@nestjs/mongoose";
 import { ConfigModule } from "@nestjs/config";
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from "@nestjs/config";
     MongooseModule.forRoot(
       `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`
     ),
+    CustomerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/customer/customer.service.ts
@@ -0,0 +1,33 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateCustomerDto } from './dto/create-customer.dto';
+import { UpdateCustomerDto } from './dto/update-customer.dto';
+import { Customer, CustomerDocument } from './entities/customer.entity';
+
+@Injectable()
+export class CustomerService {
+  constructor(
+    @InjectModel(Customer.name) private customerModel: Model<CustomerDocument>
+  ) { }
+
+  create(createCustomerDto: CreateCustomerDto) {
+    return `This action creates a customer`;
+  }
+
+  findAll() {
+    return `This action returns all customer`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} customer`;
+  }
+
+  update(id: number, updateCustomerDto: UpdateCustomerDto) {
+    return `This action updates a #${id} customer`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} customer`;
+  }
+}
